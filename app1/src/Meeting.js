8bf85bc51eae58f50fc81f253e127de93ba14f50
sun <sunpeiting07@gmail.com>
2021-11-03 07:59:17 +1030

    refactor: refactoring meeting section

--- app1/src/Meeting.js
+++ app1/src/Meeting.js
@@ -18,7 +18,22 @@ export default function Meeting() {
           </Grid>
           <Grid item sm={8} container>
             <Grid item sm={6} >
-                <MeetingItem />
+            <div class="meeting-item">
+              <div class="thumb">
+                <div class="price">
+                  <span>$22.00</span>
+                </div>
+                <a href="meeting-details.html"><img src={`${process.env.PUBLIC_URL}/images/meeting-01.jpg`} alt="New Lecturer Meeting" /></a>
+              </div>
+              <div class="down-content">
+                <div class="date">
+                  <h6>Nov <span>10</span></h6>
+                </div>
+                <a href="meeting-details.html"><h4>New Lecturers Meeting</h4></a>
+                <p>Morbi in libero blandit lectus cursus ullamcorper.</p>
+              </div>
+            </div>
+                <MeetingItem price={"22.00"}/>
               </Grid>
               <Grid item sm={6}>
                 <div class="meeting-item">
